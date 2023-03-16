d2520727a7e8420ebf3ffbdf5cd540db11183ca4
Pheeraphat12 <PBunliang12@gmail.com>
2023-03-16 13:42:49 +0000

    1

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 const toggleBtn = document.querySelector('.toggleBtn')
 const navLink = document.querySelector('.nav__links')
 
 toggleBtn.onclick = function() {
     navLink.classList.toggle('open')
 }
-=======
-const toggleBtn = document.querySelector(".icons");
-const dropDownMenu = document.querySelector(".dropdown_menu");
-
-toggleBtn.onclick = function () {
-  dropDownMenu.classList.toggle("open");
-};
->>>>>>> 880721dfd8a19398a8acc33e5067554f6e81d191
